31d71f202048161e992c9810a2a706c04a65d5d3
Roberto Dip <dip.jesusr@gmail.com>
2021-05-25 18:08:00 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <p>Hello!</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
